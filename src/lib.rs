6788c6764385d4df907464c70eb5974de8af9500
orhanbalci <orhanbalci@gmail.com>
2016-06-19 19:11:38 +0300

    refactored to use only hyper

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
-extern crate ease;
 extern crate hyper;
-
-use ease::{Url, Request, Error};
-use hyper::header::ContentType;
-use hyper::mime::{Mime, TopLevel, SubLevel};
+use hyper::*;
+use hyper::header::{Headers,Connection};
+use std::io::Read;
+use std::result::Result as StdResult;
 
 pub struct HazelcastRestClient {
     ip_address: &'static str,
     port: &'static str,
+    http_client: Client,
 }
 
 impl HazelcastRestClient {
@@ -15,101 +15,120 @@ impl HazelcastRestClient {
         HazelcastRestClient {
             ip_address: ip_address,
             port: port,
+            http_client: Client::new(),
         }
     }
 
     pub fn queue_offer<T: ToString>(self: &Self,
                                     queue_name: &str,
                                     value: T)
-                                    -> Result<String, Error> {
+                                    -> std::result::Result<String, Error> {
         let url_string = format!("http://{}:{}/hazelcast/rest/queues/{}",
                                  self.ip_address,
                                  self.port,
                                  queue_name);
-        let url = Url::parse(&url_string).unwrap();
-        Request::new(url)
-            .header(ContentType(Mime(TopLevel::Text, SubLevel::Plain, vec![])))
-            .body(value.to_string())
-            .post()
-            .and_then(|x| Ok(x.body))
+        self.http_client.post(&url_string).header(Connection::keep_alive()).body(&value.to_string()).send().and_then(|mut x| {
+            let mut content = String::new();
+            x.read_to_string(&mut content);
+            StdResult::Ok(content)
+        })
     }
 
-    pub fn queue_delete(self: &Self, queue_name: &str, timeout: i32) -> Result<String, Error> {
+    pub fn queue_delete(self: &Self,
+                        queue_name: &str,
+                        timeout: i32)
+                        -> std::result::Result<String, Error> {
         let url_string = format!("http://{}:{}/hazelcast/rest/queues/{}/{}",
                                  self.ip_address,
                                  self.port,
                                  queue_name,
                                  timeout.to_string());
-        let url = Url::parse(&url_string).unwrap();
-        Request::new(url).delete().and_then(|x| Ok(x.body))
+        self.http_client.delete(&url_string).send().and_then(|mut x| {
+            let mut content = String::new();
+            x.read_to_string(&mut content);
+            StdResult::Ok(content)
+        })
     }
 
-    pub fn queue_size(self: &Self, queue_name: &str) -> Result<String, Error> {
+    pub fn queue_size(self: &Self, queue_name: &str) -> std::result::Result<String, Error> {
         let url_string = format!("http://{}:{}/hazelcast/rest/queues/{}/size",
                                  self.ip_address,
                                  self.port,
                                  queue_name);
-        let url = Url::parse(&url_string).unwrap();
-        Request::new(url).get().and_then(|x| Ok(x.body))
+        self.http_client.get(&url_string).send().and_then(|mut x| {
+            let mut content = String::new();
+            x.read_to_string(&mut content);
+            StdResult::Ok(content)
+        })
     }
 
     pub fn map_put<T: ToString>(self: &Self,
                                 map_name: &str,
                                 key_name: &str,
                                 value: T)
-                                -> Result<String, Error> {
+                                -> std::result::Result<String, Error> {
 
         let url_string = format!("http://{}:{}/hazelcast/rest/maps/{}/{}",
                                  self.ip_address,
                                  self.port,
                                  map_name,
                                  key_name);
-        let url = Url::parse(&url_string).unwrap();
-        Request::new(url)
-            .header(ContentType(Mime(TopLevel::Text, SubLevel::Plain, vec![])))
-            .body(value.to_string())
-            .post()
-            .and_then(|x| Ok(x.body))
+        self.http_client.post(&url_string).body(&value.to_string()).send().and_then(|mut x| {
+            let mut content = String::new();
+            x.read_to_string(&mut content);
+            StdResult::Ok(content)
+        })
     }
 
-    pub fn map_get(self: &Self, map_name: &str, key_name: &str) -> Result<String, Error> {
+    pub fn map_get(self: &Self,
+                   map_name: &str,
+                   key_name: &str)
+                   -> std::result::Result<String, Error> {
 
         let url_string = format!("http://{}:{}/hazelcast/rest/maps/{}/{}",
                                  self.ip_address,
                                  self.port,
                                  map_name,
                                  key_name);
-        let url = Url::parse(&url_string).unwrap();
-        Request::new(url)
-            .header(ContentType(Mime(TopLevel::Text, SubLevel::Plain, vec![])))
-            .get()
-            .and_then(|x| Ok(x.body))
+        self.http_client.get(&url_string).send().and_then(|mut x| {
+            let mut content = String::new();
+            x.read_to_string(&mut content);
+            StdResult::Ok(content)
+        })
     }
 
-    pub fn map_remove(self: &Self, map_name: &str, key_name: &str) -> Result<String, Error> {
+    pub fn map_remove(self: &Self,
+                      map_name: &str,
+                      key_name: &str)
+                      -> std::result::Result<String, Error> {
         let url_string = format!("http://{}:{}/hazelcast/rest/maps/{}/{}",
                                  self.ip_address,
                                  self.port,
                                  map_name,
                                  key_name);
-        let url = Url::parse(&url_string).unwrap();
-        Request::new(url).delete().and_then(|x| Ok(x.body))
+        self.http_client.delete(&url_string).send().and_then(|mut x| {
+            let mut content = String::new();
+            x.read_to_string(&mut content);
+            StdResult::Ok(content)
+        })
     }
 
-    pub fn map_remove_all(self: &Self, map_name: &str) -> Result<String, Error> {
+    pub fn map_remove_all(self: &Self, map_name: &str) -> std::result::Result<String, Error> {
         let url_string = format!("http://{}:{}/hazelcast/rest/maps/{}",
                                  self.ip_address,
                                  self.port,
                                  map_name);
-        let url = Url::parse(&url_string).unwrap();
-        Request::new(url).delete().and_then(|x| Ok(x.body))
+        self.http_client.delete(&url_string).send().and_then(|mut x| {
+            let mut content = String::new();
+            x.read_to_string(&mut content);
+            StdResult::Ok(content)
+        })
     }
 }
 #[test]
 fn it_works() {
-    let client = HazelcastRestClient::new("192.168.1.23", "5701");
-    match client.queue_offer::<String>("orhan", "3".to_owned()) {
-        Ok(resp) => println!("{:?}", resp),
-        Err(err) => println!("{:?}", err),
-    }
+    let client = HazelcastRestClient::new("10.0.2.15", "5701");
+    println!("{:?}",client.queue_offer::<String>("orhan", "3".to_owned()));
+    client.queue_offer::<String>("orhan", "4".to_owned());
+    println!("{:?}", client.queue_size("orhan"));
 }
